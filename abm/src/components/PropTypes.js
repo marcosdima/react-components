97f8976cc463c22972ae3b249e0ba62c5e18b835
marcosdima <marcosdimatteo.dev@gmail.com>
2024-08-28 10:44:20 -0300

    new: PropTypes file

---
+++ abm/src/components/PropTypes.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import { fieldType } from '../enums';
+
+export const FieldProps =  {
+    type: PropTypes.oneOf(Object.values(fieldType)).isRequired,
+    name: PropTypes.string.isRequired,
+    onChange: PropTypes.func,
+    required: PropTypes.bool,
+};
+
+export const BaseFormProps = {
+    fields: PropTypes.arrayOf((PropTypes.shape(FieldProps)).isRequired),
+};
+
+export const HookShape = PropTypes.shape({
+    onChange: PropTypes.func.isRequired,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+}).isRequired;
+
+export const CharFieldProps = {
+    hook: HookShape,
+};
