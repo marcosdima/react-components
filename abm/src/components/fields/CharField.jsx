0113b871383c443ec05fc25b032bc83c7b95b541
marcosdima <marcosdimatteo.dev@gmail.com>
2024-08-29 14:12:28 -0300

    new: settings implementation on CharField and EmailField

--- abm/src/components/fields/CharField.jsx
+++ abm/src/components/fields/CharField.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import { CharFieldProps } from "../PropTypes";
+import statusType from "../../enums/statusType";
 
-const CharField = ({ hook }) => {
+const CharField = ({ hook, status, settings: { maxLength, minLength } }) => {
+    useEffect(() => {
+        if (maxLength && hook.value.length > maxLength) {
+            status(statusType.ERROR, `'${hook.name}' has a limit of ${maxLength} characters.`);
+        } else if (minLength && hook.value.length < minLength) {
+            status(statusType.ERROR, `'${hook.name}' has a minimum of ${minLength} characters.`);
+        } else {
+            status(statusType.OK, `'${hook.name}' it's OK!`);
+        }
+    }, [status, hook.value, hook.name, maxLength, minLength]);
+
     return (
         <>
             <label>{hook.name}</label>
--- abm/src/components/fields/EmailField.jsx
+++ abm/src/components/fields/EmailField.jsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import { CharFieldProps } from "../PropTypes";
+import statusType from "../../enums/statusType";
 
-const EmailField = ({ hook }) => {
+const EmailField = ({ hook, status, settings: { validDomains } }) => {
+    useEffect(() => {
+        const email = hook.value;
+        const [, emailDomain] = email.split('@');
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        
+        if (!regex.test(email)) {
+            status(statusType.ERROR, `'${hook.name}' does not have a valid format.`);
+        } else if (validDomains && !validDomains.find((domain) => emailDomain.startsWith(domain))) {
+            status(statusType.ERROR, `'${hook.name}' does not have a valid domain.`);
+        } else {
+            status(statusType.OK, `'${hook.name}' it's OK!`);
+        }
+    }, [hook.name, hook.value, hook.value.length, status, validDomains]);
+
     return (
         <>
             <label>{hook.name}</label>
