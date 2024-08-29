806f08ddb7b38f67a8576c46aeae634beb190f34
marcosdima <marcosdimatteo.dev@gmail.com>
2024-08-29 14:12:44 -0300

    new: add EmailField to index file

--- abm/src/components/fields/index.js
+++ abm/src/components/fields/index.js
@@ -1,8 +1,10 @@
 import CharField from "./CharField";
+import EmailField from "./EmailField";
 import fieldType from "../../enums/fieldType";
 
 const fields = {
     [fieldType.CHAR]: CharField,
+    [fieldType.EMAIL]: EmailField,
 };
 
 export default fields;
