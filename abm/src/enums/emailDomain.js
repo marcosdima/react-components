f4f022e61f5f141ca715926087d490fe41e7ad03
marcosdima <marcosdimatteo.dev@gmail.com>
2024-08-29 11:45:57 -0300

    new: enums emailDomains and statusType

---
+++ abm/src/enums/emailDomain.js
@@ -0,0 +1,10 @@
+const emailDomain = Object.freeze({
+    GMAIL: 'gmail.com',
+    YAHOO: 'yahoo.com',
+    OUTLOOK: 'outlook.com',
+    HOTMAIL: 'hotmail.com',
+    ICLOUD: 'icloud.com',
+    ZOHO: 'zoho.com',
+});
+
+export default emailDomain;
---
+++ abm/src/enums/statusType.js
@@ -0,0 +1,7 @@
+const statusType = Object.freeze({
+    ERROR: 'error',
+    WARNING: 'warning',
+    OK: 'ok',
+});
+
+export default statusType;
