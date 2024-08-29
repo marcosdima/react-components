eab8894fbca14c8fe89cdbbf6d556277411aa07e
marcosdima <marcosdimatteo.dev@gmail.com>
2024-08-29 14:14:36 -0300

    new: update status

--- abm/src/components/Field.jsx
+++ abm/src/components/Field.jsx
@@ -1,16 +1,36 @@
-/* eslint-disable no-unused-vars */
-import { useEffect } from 'react';
-import useField from '../hooks/useField';
+import { useCallback, useEffect, useState } from 'react';
 import { FieldProps } from './PropTypes';
 import fields from './fields';
+import useField from '../hooks/useField';
+import statusType from '../enums/statusType';
 
-const Field = ({ type, name, onChange, required = false }) => {
-    const field = useField(name);
+const Field = ({ type, name, onChange, required = false, settings={} }) => {
+    const input = useField(name);
+    const [status, setStatus] = useState({
+        status: '',
+        statusMessage: '',
+    });
 
     // Every time field is modified, sends the data to form using the onChange function.
     useEffect(() => {
-        onChange(name, field.value);
-    }, [onChange, field.value, name]);
+        if (required && !input.value) {
+            onChange(
+                name, 
+                input.value, 
+                {
+                    status: statusType.ERROR,
+                    statusMessage: `${name} is required`,
+                },
+            );
+        } else {
+            onChange(name, input.value, status);
+        }
+    }, [onChange, input.value, name, status, required]);
+
+
+    const updateStatus = useCallback((status, statusMessage) => {
+        setStatus({ status, statusMessage });
+    }, []);
 
     const FieldComponent = fields[type];
     
@@ -18,7 +38,7 @@ const Field = ({ type, name, onChange, required = false }) => {
         return <p>Error: component {type} does not exists</p>;
     }
 
-    return <FieldComponent hook={field} />;
+    return <FieldComponent hook={input} status={updateStatus} settings={settings}/>;
 };
 
 Field.propTypes = FieldProps;
