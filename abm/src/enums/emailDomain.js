ef32caf44c3f7027d64708f0f646f6a9aebf0d01
marcosdima <marcosdimatteo.dev@gmail.com>
2024-08-30 16:08:40 -0300

    new: comment TODO

--- abm/src/enums/emailDomain.js
+++ abm/src/enums/emailDomain.js
@@ -1,3 +1,4 @@
+// TODO: maybe it could has regex to check. Example: gmail.com.ar is not valid, but yahoo.com.ar it's right.
 const emailDomain = Object.freeze({
     GMAIL: 'gmail.com',
     YAHOO: 'yahoo.com',
