a406061c009e76d66baa18a799d9288c7d9cd84c
marcosdima <marcosdimatteo.dev@gmail.com>
2024-11-06 10:03:48 -0300

    mod: input container

--- abm/src/styles/Input.styles.js
+++ abm/src/styles/Input.styles.js
@@ -1,17 +1,27 @@
 import styled from 'styled-components';
 
 const StyledInput = styled.input`
-    padding: 10px;
-    font-size: 16px;
-    border: 2px solid #ccc;
-    border-radius: 5px;
-    outline: none;
-    transition: border-color 0.3s ease;
-    margin-bottom: 8px;
+  padding: 10px;
+  font-size: 16px;
+  border: 2px solid #ccc;
+  border-radius: 5px;
+  outline: none;
+  transition: border-color 0.3s ease;
+  margin-bottom: 8px;
 
-    &:focus {
-        border-color: #4C4B16;
-    }
+  &:focus {
+      border-color: #4C4B16;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+  justify-content: space-between;
+  padding-bottom: 18px;
 `;
 
 export default StyledInput;
