fe20a8c28e41081a423c9ac3ca0a5afca0226cde
marcosdima <marcosdimatteo.dev@gmail.com>
2024-11-06 10:02:30 -0300

    mod: label prop field

--- abm/src/components/elements/InputCustom.jsx
+++ abm/src/components/elements/InputCustom.jsx
@@ -1,8 +1,14 @@
-import StyledInput from "../../styles/Input.styles";
+import StyledInput, { Container } from "../../styles/Input.styles";
 import { InputCustomProps } from "../PropTypes";
+import LabelCustom from "./LabelCustom";
 
-const InputCustom = ({ ...props }) => {
-    return (<StyledInput {...props}/>);
+const InputCustom = ({ label, ...props }) => {
+    return (
+        <Container>
+            <LabelCustom {...label}/>
+            <StyledInput {...props} />
+        </Container>
+    );
 };
 
 InputCustom.propTypes = InputCustomProps;
--- abm/src/components/elements/TextAreaCustom.jsx
+++ abm/src/components/elements/TextAreaCustom.jsx
@@ -1,8 +1,15 @@
+import { Container } from "../../styles/Input.styles";
 import StyledTextArea from "../../styles/TextArea.styles";
 import { TextAreaCustomProps } from "../PropTypes";
+import LabelCustom from "./LabelCustom";
 
-const TextAreaCustom = ({ ...props }) => {
-    return (<StyledTextArea {...props} />);
+const TextAreaCustom = ({ label, ...props }) => {
+    return (
+        <Container>
+            <LabelCustom {...label}/>
+            <StyledTextArea {...props} />
+        </Container>
+    );
 };
 
 TextAreaCustom.propTypes = TextAreaCustomProps;
