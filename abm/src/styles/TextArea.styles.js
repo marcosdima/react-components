abd941d5afeb6bfdc4c8965281554669d26302a8
marcosdima <marcosdimatteo.dev@gmail.com>
2024-11-09 10:03:07 -0300

    mod: style change

--- abm/src/styles/TextArea.styles.js
+++ abm/src/styles/TextArea.styles.js
@@ -2,13 +2,11 @@ import styled from 'styled-components';
 
 const StyledTextArea = styled.textarea`
     padding: 10px;
-    font-size: 16px;
     border: 2px solid #ccc;
     border-radius: 5px;
     outline: none;
     transition: border-color 0.3s ease;
     margin-bottom: 8px;
-    resize: vertical;
 
     &:focus {
         border-color: #4C4B16;
