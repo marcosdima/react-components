77a965741a090ebbe300384c5cfe2615a0d38904
marcosdima <marcosdimatteo.dev@gmail.com>
2024-11-11 10:50:14 -0300

    new: App shows functionallity

--- abm/src/App.jsx
+++ abm/src/App.jsx
@@ -1,8 +1,111 @@
+import BaseForm from "./components/BasicForm";
+import emailDomain from "./enums/emailDomain";
+import fieldType from "./enums/fieldType";
+
 function App() {
+  const fields = [
+    {
+      type: fieldType.CHAR,
+      name: 'username',
+      required:true,
+      placeholder: 'Ingrese un username...',
+      row: 0,
+      col: 0,
+    },
+    {
+      type: fieldType.CHAR,
+      name: 'firstname',
+      required:true,
+      settings: {
+          maxLength: 4,
+      },
+      row: 0,
+      col: 1,
+    },
+    {
+      type: fieldType.EMAIL,
+      name: 'e-mail',
+      required:true,
+      settings: {
+        validDomains: [emailDomain.GMAIL],
+      },
+      row: 0,
+      col: 2,
+    },
+    {
+      type: fieldType.NUMBER,
+      name: 'age',
+      required:true,
+      placeholder: '18...110',
+      settings: {
+        min: 18,
+        max: 110,
+      },
+      row: 0,
+      col: 3,
+    },
+    {
+      type: fieldType.IMAGE,
+      name: 'perfil',
+      required:true,
+      row: 1,
+      col: 0,
+    },
+    {
+      type: fieldType.PASSWORD,
+      name: 'password',
+      settings: {
+        minLength: 8,
+        alphaNumeric: true,
+        signs: true,
+      },
+      required:true,
+      row: 1,
+      col: 1,
+    },
+    {
+      type: fieldType.SELECTOR,
+      name: 'estado',
+      required: true,
+      settings: {
+          values: [{label: 'casa', value: 1}, {label: 'roto', value: 2}],
+      },
+      row: 1,
+      col: 2,
+    },
+    {
+      type: fieldType.TEXT_AREA,
+      name: 'descripcion',
+      required: true,
+      placeholder: 'Escriba una descripción...',
+      settings: {
+        minLength: 4,
+      },
+      row: 2,
+      col: 0,
+    },
+    {
+      type: fieldType.SEARCH_LOCATION,
+      name: 'location',
+      required: true,
+      settings: {
+          appendAtEnd: 'Buenos Aires, Argentina',
+          defaultValue: 'Las Heras 1, Luján',
+      },
+      row: 2,
+      col: 1,
+    },
+  ];
+  
   return (
-    <>
-    </>
-  )
+    <BaseForm
+      fields={fields}
+      // eslint-disable-next-line no-console
+      onSubmit={(data) => console.log(data)}
+      style={{width: '100%', heigth: '100%'}}
+      language="EN"
+    />
+  );
 }
 
-export default App
+export default App;
